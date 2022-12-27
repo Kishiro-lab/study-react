98ffe40c726d2c1349a60601b720e5d7cc725643
kishiro <the.yuhei@gmail.com>
2022-12-27 16:04:03 +0900

    ✨ カウンターを設置してuseStateでカウントアップする

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,17 @@
 import Head from 'next/head'
-import { useCallback, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Main } from 'src/components/Main'
 
 export default function Home() {
-  const foo = 1;
-
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  }, []);
+  const [count, setCount] = useState(1);
+  const handleClick = (e) => {
+    setCount(count => count + 1);
+    setCount(count => count + 1);
+  };
 
   useEffect(() => {
-    console.log("mount");
     document.body.style.backgroundColor = "lightblue";
     return () => {
-      console.log("unmount");
       document.body.style.backgroundColor = "";
     };
   }, []);
@@ -25,11 +21,11 @@ export default function Home() {
       <Head>
         <title>Index Page</title>
       </Head>
-      <a 
-        href="/about"
+      <h1>{count}</h1>
+      <button
         onClick={handleClick}>
         ボタン
-      </a>
+      </button>
       <Main page="index" />
     </>
   )
