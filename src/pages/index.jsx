0489c842833686f24e33b6adcbf5269b96d43a38
kishiro <the.yuhei@gmail.com>
2022-12-27 09:58:56 +0900

    ⚗️クリックイベントについての学習。いつか消す。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,26 @@
 import Head from 'next/head'
+import { useCallback } from 'react';
 import { Main } from 'src/components/Main'
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <>
       <Head>
         <title>Index Page</title>
       </Head>
+      <a 
+        href="/about"
+        onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index" />
     </>
   )
