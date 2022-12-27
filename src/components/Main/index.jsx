d12210d2acc9e264086d958c8ab60e04b5d8484c
kishiro <the.yuhei@gmail.com>
2022-12-27 21:00:28 +0900

    ✨ 文字列と真偽値についてuseState

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -2,14 +2,52 @@ import classes from 'src/components/Main/Main.module.css'
 import { Header } from "src/components/Header"
 import { Center } from "src/components/Center"
 import { Footer } from 'src/components/Footer'
+import { useCallback, useEffect, useState } from 'react';
 
 export function Main(props) {
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState(" ");
+  const [isShow, setIsShow] = useState(true);
+
+  const handleClick = useCallback(() => {
+    if( count < 10) {
+      setCount(count => count + 1);
+    }
+  }, [count]);
+
+  const handleDisplay = useCallback(() =>{
+    setIsShow((isShow) => !isShow);
+  }, []);
+  
+  const handleChange = useCallback(e => {
+    if (e.target.value.length > 5 ) {
+      alert("5文字以内にしてください。");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
+  
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
+
   return (
       <main className={classes.main}>
         <Header>
           {<code className={classes.code}>pages/{props.page}.js</code>}
         </Header>
         <Center page={props.page}/>
+        {isShow ? <h1>{count}</h1> : null}
+      <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <input 
+      type="text" 
+      value={text} 
+      onChange={ handleChange } 
+      />
         <Footer />
       </main>
   )
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,33 +1,13 @@
 import Head from 'next/head'
-import { useCallback, useEffect, useState } from 'react';
 import { Main } from 'src/components/Main'
 
 export default function Home() {
-  const [count, setCount] = useState(1);
-
-  const handleClick = useCallback(() => {
-    if( count < 10) {
-      setCount(count => count + 1);
-    }
-  }, [count]);
-  
-  useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
-    return () => {
-      document.body.style.backgroundColor = "";
-    };
-  }, []);
 
   return (
     <>
       <Head>
         <title>Index Page</title>
       </Head>
-      <h1>{count}</h1>
-      <button
-        onClick={handleClick}>
-        ボタン
-      </button>
       <Main page="index" />
     </>
   )
